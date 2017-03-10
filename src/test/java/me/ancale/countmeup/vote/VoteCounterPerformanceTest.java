90f00f24311a27a957e98b5b7bdf606177230a18
ancaleuca <ancaleuca2005@gmail.com>
2017-03-10 15:42:30 +0000

    Basic performance test

---
+++ src/test/java/me/ancale/countmeup/vote/VoteCounterPerformanceTest.java
@@ -0,0 +1,27 @@
+package me.ancale.countmeup.vote;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class VoteCounterPerformanceTest {
+
+    public static void main(String... args) {
+        System.out.println("Time start " + LocalDateTime.now());
+        VoteCounter counter = new InMemoryVoteCounter(originalVotes());
+        System.out.println("Time start counting " + LocalDateTime.now());
+        Map<String, Long> votesPerCandidate = counter.countTotalAccountablePerCandidate();
+        System.out.println(votesPerCandidate);
+        System.out.println("Time end " + LocalDateTime.now());
+    }
+
+    private static Set<Vote> originalVotes() {
+        int count = 10000000;
+        Set<Vote> votes = new HashSet<>(count);
+        for (int i = 0; i < count; i++) {
+            votes.add(new Vote("u" + i, "1", LocalDateTime.now()));
+        }
+        return votes;
+    }
+}
