275a59541aaf893e9643cafd2c1b588844da5885
ancaleuca <ancaleuca2005@gmail.com>
2017-03-11 08:34:49 +0000

    Make performance test an actual test and using threads.

--- src/test/java/me/ancale/countmeup/vote/VoteCounterPerformanceTest.java
+++ src/test/java/me/ancale/countmeup/vote/VoteCounterPerformanceTest.java
@@ -1,26 +1,66 @@
 package me.ancale.countmeup.vote;
 
+import org.junit.Test;
+
 import java.time.Instant;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 public class VoteCounterPerformanceTest {
 
-    public static void main(String... args) {
-        System.out.println("Time start " + LocalDateTime.now());
-        int count = 10_000_000;
+    @Test
+    public void multipleThreadsShouldCountVotesUnderOneSecond() throws Exception {
         InMemoryVoteCounter inMemoryVoteCounter = new InMemoryVoteCounter();
+        int totalExpectedCount = 10_000_000;
+        int totalAddingThreads = 100;
+        List<Thread> threads = new ArrayList<>(totalAddingThreads);
+
+        for (int i = 0; i < totalAddingThreads; i++) {
+            threads.add(new Thread(() -> addVotes(totalExpectedCount / totalAddingThreads, inMemoryVoteCounter)));
+        }
+
+        Thread countingThread = new Thread(() -> countVotes(inMemoryVoteCounter));
+
+        threads.add(countingThread);
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        assertThat(inMemoryVoteCounter.count().getTotalVotes(), is((long)totalExpectedCount));
+    }
+
+    private void addVotes(int count, InMemoryVoteCounter inMemoryVoteCounter) {
         for (int i = 0; i < count; i++) {
             Vote vote = new Vote("u" + i, "c" + i % 5, Instant.now());
             inMemoryVoteCounter.addVote(vote);
-            if (i % 300_000 == 0) {
-                Instant start = Instant.now();
-                VoteCountSummary summary = inMemoryVoteCounter.count();
-                Instant end = Instant.now();
-                System.out.println("Time spent counting (ms): " + (end.toEpochMilli() - start.toEpochMilli()));
-                System.out.println("Total votes so far: " + summary.getTotalVotes());
-            }
         }
-        System.out.println("Total votes: " + inMemoryVoteCounter.count().getTotalVotes());
-        System.out.println("Time end " + LocalDateTime.now());
+    }
+
+    private void countVotes(InMemoryVoteCounter inMemoryVoteCounter) {
+        int numberOfRuns = 30;
+        int sleepForMillis = 1000;
+
+        int i = 0;
+        while(i++ <= numberOfRuns) {
+            try {
+                Thread.sleep(sleepForMillis);
+            } catch (InterruptedException e) {
+                fail(e.getMessage());
+            }
+            Instant start = Instant.now();
+            inMemoryVoteCounter.count();
+            Instant end = Instant.now();
+            long millisSpentCounting = end.toEpochMilli() - start.toEpochMilli();
+            assertThat("Took over a second to count = " + millisSpentCounting, millisSpentCounting < 1000, is(true));
+        }
     }
 }
