b77cf56f94c2b42679e14c2fe2834394ba89cf6e
ancaleuca <ancaleuca2005@gmail.com>
2017-03-12 11:36:51 +0000

    “Test” to load votes in the database faster

---
+++ src/test/java/me/ancale/countmeup/performance/LoadInitialDataTest.java
@@ -0,0 +1,40 @@
+package me.ancale.countmeup.performance;
+
+import me.ancale.countmeup.model.vote.Vote;
+import me.ancale.countmeup.service.VoteStore;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.Instant;
+import java.util.Random;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = MOCK)
+@AutoConfigureMockMvc
+public class LoadInitialDataTest {
+
+    @Autowired
+    private VoteStore voteStore;
+
+    @Test
+    @Ignore
+    public void loadData() {
+        Random random = new Random(100);
+        int voteCount = 1_000_000;
+        int userCount = 800_000;
+
+        for (int i = 0; i < voteCount; i++) {
+            String userId = "u" + random.nextInt(userCount);
+            String candidateId = "c" + (1 + random.nextInt(4));
+            voteStore.addVote(new Vote(userId, candidateId, Instant.now()));
+        }
+    }
+
+}
